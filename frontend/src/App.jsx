c0eec7c59b2ab5a0dc1b6b92023f4b39419f5c62
Antoine Gousset <ant.gousset@gmail.com>
2026-07-01 21:23:00 +0200

    style: harmonize visual system across app pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import AppRoutes from "./routes/AppRoutes";
 import "./App.css";
 import "./styles/20-global-background.css";
+import "./styles/23-global-visual-harmonization.css";
 
 function App() {
   return <AppRoutes />;
